18ff009a0c955e019501a7a839b73bef5eb3f5a2
minhhoang1220 <21070784@vnu.edu.vb>
2024-10-17 17:37:44 +0700

    Add rails admin

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :movies
   resources :theatres
   resources :movie_shows
